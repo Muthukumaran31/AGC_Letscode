c35c3a22b3233fd31729644153b9811f7f74fa87
Muthukumaran31 <muthukumarand08@gmail.com>
2024-06-26 12:35:52 +0530

    Adding the data type file to git hub

---
+++ Letcode/src/main/java/datatype/Learner.java
@@ -0,0 +1,17 @@
+package datatype;
+
+
+public class Learner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "muthukumaran.D";
+		System.out.println(name);
+		byte age = 28;
+		 System.out.println(age);
+		 float height = 165.102f;
+		 System.out.println(height);
+
+	}
+
+}

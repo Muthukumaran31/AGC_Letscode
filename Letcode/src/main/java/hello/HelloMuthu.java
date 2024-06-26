6970ce8ccd9f34c022545ce12ddf4f8f753f4580
Muthukumaran31 <muthukumarand08@gmail.com>
2024-06-26 11:41:26 +0530

    Added more codes

--- Letcode/src/main/java/hello/HelloMuthu.java
+++ Letcode/src/main/java/hello/HelloMuthu.java
@@ -9,3 +9,44 @@ public class HelloMuthu {
 	}
 
 }
+
+
+
+
+//public class HelloMuthu
+//{
+//	public static void main(String[] args)
+//	{
+//	String message="helloworld";
+//	System.out.println(message);
+//	}
+//}
+
+
+
+
+
+
+
+
+//public class HelloMuthu
+//{
+//	public static void main(String[] args)
+//	{
+//		System.out.println("Muthu try 3");
+//	}
+//	}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
